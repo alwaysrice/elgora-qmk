ca7ab55bfb7789b94f21252421941b82042f1a0a
AlwaysRice <divineswordbearer@gmail.com>
2026-02-04 09:39:52 +0800

    New taller model

--- keymaps/multi/keymap.c
+++ keymaps/multi/keymap.c
@@ -19,36 +19,37 @@ const uint16_t VOL_DOWN = KC_KB_VOLUME_DOWN;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT(
-        KC_GRV,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,				KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,
-        KC_LBRC,	KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,				KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_RBRC,
-        KC_LALT,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,				KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_RALT,
-        KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,				KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,
-        KC_TAB,		KC_SPC,		FUNC,		KC_LCTL,															KC_SPC,		KC_EQL,	    KC_QUOT,	KC_BSLS,  
-        MIRROR,		MOUSE,		KC_LGUI,																					KC_BSPC,	KC_ENT,		MOUSE
+        KC_GRV,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_6,				KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,	KC_EQL,		KC_BSPC,
+        KC_LBRC,	KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_UP,				KC_UP,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_RBRC,
+        KC_LALT,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_DOWN,			KC_DOWN,	KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	MT(MOD_RALT, KC_QUOT),
+        KC_LSFT,	MOUSE,		KC_X,		KC_C,		KC_V,		KC_B,										KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	MT(MOD_RSFT, KC_BSLS),
+        KC_SPC,		KC_TAB,		KC_LEFT,	KC_RGHT,	KC_TAB,																KC_TAB,		KC_RIGHT,	KC_LEFT,	KC_TAB,		KC_SPC,
+        KC_LCTL,	KC_LGUI,	FUNC,		MIRROR,																						KC_ENT,		KC_SPC,	    KC_SPC,		KC_SPC
     ),
     [LAYER_MIRROR] = LAYOUT(
-        KC_MINS,	KC_0,		KC_9,		KC_8,		KC_7,		KC_6,				_______,	_______,	_______,	_______,	_______,	_______,
-        KC_RBRC,	KC_P,		KC_O,		KC_I,		KC_U,		KC_Y,				_______,	_______,	KC_UP,		_______,	_______,	_______,
-        _______,	KC_SCLN,	KC_L,		KC_K,		KC_J,		KC_H,				_______,	KC_LEFT,	KC_DOWN,	KC_RGHT,	_______,	_______,
-        _______,	KC_SLSH,	KC_DOT,		KC_COMM,	KC_M,		KC_N,				_______,	_______,	_______,	_______,	_______,	_______,
-        KC_BSLS,	KC_QUOT,	KC_EQL,	    _______,															_______,	_______,	_______,	_______,  
-        _______,	KC_BSPC,	_______,																					_______,	_______,	_______
+        KC_BSPC,	KC_EQL,		KC_MINS,	KC_0,		KC_9,		KC_8,		KC_7,				_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        KC_RBRC,	KC_P,		KC_O,		KC_I,		KC_U,		KC_Y,		KC_QUOT,			_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	KC_SCLN,	KC_L,		KC_K,		KC_J,		KC_H,		KC_BSLS,			_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	KC_SLSH,	KC_DOT,		KC_COMM,	KC_M,		KC_N,										_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,	_______,	_______,															_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,	_______,																					_______,	_______,	_______,	_______
+
     ),
     [LAYER_FUNC] = LAYOUT(
-        KC_F6,		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,				_______,	_______,	_______,	_______,	_______,	_______,
-        _______,	KC_F10,		_______,	KC_UP,		_______,	MEH(KC_F6),			_______,	_______,	KC_UP,		_______,	_______,	_______,
-        _______,	KC_F11,		KC_LEFT,	KC_DOWN,	KC_RGHT,	MEH(KC_F5),			_______,	KC_LEFT,	KC_DOWN,	KC_RGHT,	_______,	_______,
-        _______,	KC_F12,		MEH(KC_F1),	MEH(KC_F2),	MEH(KC_F3),	MEH(KC_F4),			_______,	_______,	_______,	_______,	_______,	_______,
-        _______,	_______,	_______,	_______,												_______,	_______,	_______,	_______,  
-        _______,	_______,	_______,																					_______,	_______,	_______
+        _______,	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_6,				_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,				_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	MEH(KC_F1),	MEH(KC_F2),	MEH(KC_F3),	MEH(KC_F4),	MEH(KC_F5),	MEH(KC_F6),			_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	MEH(KC_F7),	MEH(KC_F8),	MEH(KC_F9),	MEH(KC_F10),MEH(KC_F11),								_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,	_______,																		_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,																								_______,	_______,	_______,	_______
     ),
     [LAYER_MOUSE] = LAYOUT(
-        KC_CAPS,	MEH(KC_F7),	MEH(KC_F8),	MEH(KC_F9),	MEH(KC_F10),MEH(KC_F11),		_______,	_______,	_______,	_______,	_______,	_______,
-        KC_BRIU,	VOL_UP,		_______,	MS_WHLU,	_______,	KC_F7,  			_______,	_______,	KC_UP,		_______,	_______,	_______,
-        KC_BRID,	VOL_DOWN,	MS_WHLL,	MS_WHLD,	MS_WHLR,	KC_F8,		    	_______,	KC_LEFT,	KC_DOWN,	KC_RGHT,	_______,	_______,
-        KC_PSCR,	_______,	MS_BTN1,	MS_BTN3,	MS_BTN2,	KC_F9,		    	_______,	_______,	_______,	_______,	_______,	_______,
-        _______,	_______,	_______,	_______,															_______,	_______,	_______,	_______,  
-        KC_LCTL,	_______,	_______,																					_______,	_______,	_______
+        _______,	_______,	_______,	_______,	_______,	_______,	_______,			_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,	MS_WHLU,	_______,	_______, 	_______,			_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	MS_WHLL,	MS_WHLD,	MS_WHLR,	_______,	_______,	    	_______,	_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	MS_BTN1,	MS_BTN3,	MS_BTN2,	_______,		    						_______,	_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,	_______,																		_______,	_______,	_______,	_______,	_______,
+        _______,	_______,	_______,																								_______,	_______,	_______,	_______
     )
     // [TRNS] = LAYOUT(
     //     _______,  _______,   _______,    _______,    _______,    _______,       _______,    _______,    _______,    _______,    _______,    _______,
